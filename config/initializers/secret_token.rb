423e84d227f1373f7bd1f641bc804d5571c7e1f3
David E. Smyth, Chief Scientist <David.Smyth@Webair.com>
2013-03-04 20:19:44 -0800

    Initial commit, all built by "rails new"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '28884f4c286f1b5b5ffd05dede7c392510109fe060f27f6578f0146fcee8a3079d2918210e803952e6ac16660d3b3f774e5b439b0013d9c4ffd17afebbb17ba8'
